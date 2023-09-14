eb49140591adb7e9800e21f9665e704cc498f95f
Sarita Joshi <43993824+Sarita-Joshi@users.noreply.github.com>
2023-09-15 00:32:35 +0530

    name changes

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -10,7 +10,6 @@ const Landing = () => {
         <div className={styles.section_title}>
             {info.map(i =>  <div className={styles.listing} key={i.title}>
                                 <img className={styles.listing_img} src={i.img} alt={i.title} onClick={() => history.push("/"+ i.title)}/>
-                                <div className="watermark">watermarkText</div>
                                 <div className={styles.listing_text} onClick={() => history.push("/"+ i.title)}>
                                     <div className={styles.text}>
                                         <h3>{i.title}</h3>
